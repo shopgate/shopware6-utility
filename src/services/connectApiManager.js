9b4a10247c8bfd4ec5949aa7276af055f12b275a
Konstantin K <k@componentk.com>
2022-10-12 17:29:49 +0300

    SW6M-56 changed sgconnect slug to sgwebcheckout

--- src/services/connectApiManager.js
+++ src/services/connectApiManager.js
@@ -2,7 +2,8 @@
 
 const { invokeGet } = require('@shopware-pwa/shopware-6-client')
 
-const getLoginTokenEndpoint = () => '/store-api/sgconnect/login/token'
+const getLoginEndpoint = () => 'sgwebcheckout/login'
+const getLoginTokenEndpoint = () => '/store-api/sgwebcheckout/login/token'
 
 /**
  * @param {ApiteSW6Utility.SWApiInstance?} api
@@ -10,4 +11,16 @@ const getLoginTokenEndpoint = () => '/store-api/sgconnect/login/token'
  */
 const getLoginToken = async (api) => invokeGet({ address: getLoginTokenEndpoint() }, api).then(({ data }) => data)
 
-module.exports = { getLoginToken }
+/**
+ * @param {string} baseUrl - the base Url to append login controller, e.g. http://example.com
+ * @param {object} props - key/value pairs for Url parameters
+ * @returns {URL}
+ */
+const getLoginUrl = (baseUrl, props) => {
+  const url = new URL(getLoginEndpoint(), baseUrl)
+  Object.keys(props).forEach(key => url.searchParams.append(key, props[key]))
+
+  return url
+}
+
+module.exports = { getLoginToken, getLoginUrl }
